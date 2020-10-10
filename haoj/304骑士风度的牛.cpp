1d2abb86ecdb75dbc230de5a592cbe6976a66068
luckzhangfengbo <1819067326@qq.com>
2020-10-10 16:26:10 +0800

    新文件：   "304\351\252\221\345\243\253\351\243\216\345\272\246\347\232\204\347\211\233.cpp" 	修改：     a.out

---
+++ haoj/304骑士风度的牛.cpp
@@ -0,0 +1,67 @@
+/*************************************************************************
+ > File Name: 304骑士风度的牛.cpp
+ > Author: 
+ > Mail: 
+ > Created Time: 六 10/10 16:14:20 2020
+ ************************************************************************/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <queue>
+#include <map>
+#include <stack>
+#include <cstring>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+using namespace std;
+
+
+int n, m;
+
+struct node {
+    int x, y, step;
+};
+
+queue<node> que;
+char mmap[200][200];
+
+int dir[8][2]  = {
+    1, 2, 1,  -2,  2, 1, 2, -1,
+    -1, 2, -1, -2,  -2,  1, -2, -1 
+};
+
+
+int main() {
+    cin >> m >>   n;
+    for (int i = 1; i <=  n; i++) {
+        for (int j = 1; j <= m; j++) {
+            cin >> mmap[i][j];
+            if (mmap[i][j] == 'K') {
+                que.push({i , j, 0});
+            }
+        }
+    }
+
+    while (!que.empty()) {
+        node temp = que.front();
+        que.pop();
+        for (int i = 0; i < 8; i++) {
+            int x = temp.x + dir[i][0];
+            int y = temp.y + dir[i][1];
+            if (x < 1 || y < 1 || x > n || y > m) continue;
+            if (mmap[x][y] == 'H') {
+                cout << temp.step + 1 << endl;
+                return 0;
+            }
+            if (mmap[x][y] == '.') {
+                mmap[x][y] = 0;
+                que.push({x, y, temp.step + 1});
+            }
+        }
+    }
+
+    return 0;
+}
